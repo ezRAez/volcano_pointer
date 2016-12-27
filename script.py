a9e7d31206f82980c8342ff5df8180b7fccbdc9a
Ezra Raez <ezRAez10@gmail.com>
2016-12-27 13:22:42 -0800

    add different colors for volcanoes;

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ vol_df = pandas.read_csv("data/Volcanoes-USA.txt")
 
 map = folium.Map(location=[45.372, -121.697], zoom_start=10, tiles="Stamen Terrain")
 
-for lat,lon,name in zip(vol_df["LAT"], vol_df["LON"], vol_df["NAME"]):
-    map.simple_marker(location=[lat, lon], popup=name, marker_color="red")
+for lat,lon,name, elev in zip(vol_df["LAT"], vol_df["LON"], vol_df["NAME"], vol_df["ELEV"]):
+    map.simple_marker(location=[lat, lon], popup=name, marker_color="green" if elev in range(0,1000) else 'orange' if elev in range(1000,3000) else "red")
 
 map.create_map(path="index.html")
