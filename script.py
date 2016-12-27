70d4b7a1df2324d79de90c33ac46c8a67f997930
Ezra Raez <ezRAez10@gmail.com>
2016-12-27 13:47:14 -0800

    remove dep warnings

--- script.py
+++ script.py
@@ -6,6 +6,7 @@ vol_df = pandas.read_csv("data/Volcanoes-USA.txt")
 avg_lat = vol_df['LAT'].mean()
 avg_lon = vol_df['LON'].mean()
 
+map = folium.Map(location=[avg_lat, avg_lon], zoom_start=6, tiles="Stamen Terrain")
 
 def color_picker(elevation):
     minimum = int(min(vol_df['ELEV']))
@@ -17,9 +18,7 @@ def color_picker(elevation):
     else:
         return "red"
 
-map = folium.Map(location=[avg_lat, avg_lon], zoom_start=6, tiles="Stamen Terrain")
-
 for lat,lon,name, elev in zip(vol_df["LAT"], vol_df["LON"], vol_df["NAME"], vol_df["ELEV"]):
-    map.simple_marker(location=[lat, lon], popup=name, marker_color=color_picker(elev))
+    map.add_child(folium.Marker(location=[lat, lon], popup=name, icon=folium.Icon(color=color_picker(elev))))
 
-map.create_map(path="index.html")
+map.save(outfile="index.html")
